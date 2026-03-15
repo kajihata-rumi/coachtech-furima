fce8e4e118bdc74d02212e9c8aaf514209fedc3e
kajihata-rumi <owlish.jp@gmail.com>
2026-03-15 20:04:28 +0900

    Implement auth flow and style register page

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>会員登録</title>
+    <link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
+</head>
+
+<body>
+    <div class="auth-page">
+        <div class="auth-header">
+            <div class="auth-header__logo">COACHTECH</div>
+        </div>
+
+        <div class="auth-container">
+            <h1 class="auth-title">会員登録</h1>
+
+            @if ($errors->any())
+                <div class="auth-error">
+                    <ul class="auth-error__list">
+                        @foreach ($errors->all() as $error)
+                            <li class="auth-error__item">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form class="auth-form" method="POST" action="{{ route('register') }}">
+                @csrf
+
+                <div class="auth-form__group">
+                    <label class="auth-form__label" for="name">ユーザー名</label>
+                    <input class="auth-form__input" id="name" type="text" name="name" value="{{ old('name') }}" required
+                        autofocus>
+                </div>
+
+                <div class="auth-form__group">
+                    <label class="auth-form__label" for="email">メールアドレス</label>
+                    <input class="auth-form__input" id="email" type="email" name="email" value="{{ old('email') }}"
+                        required>
+                </div>
+
+                <div class="auth-form__group">
+                    <label class="auth-form__label" for="password">パスワード</label>
+                    <input class="auth-form__input" id="password" type="password" name="password" required
+                        autocomplete="new-password">
+                </div>
+
+                <div class="auth-form__group">
+                    <label class="auth-form__label" for="password_confirmation">確認用パスワード</label>
+                    <input class="auth-form__input" id="password_confirmation" type="password"
+                        name="password_confirmation" required>
+                </div>
+
+                <button class="auth-form__button" type="submit">登録する</button>
+
+                <div class="auth-form__link-wrap">
+                    <a class="auth-form__link" href="{{ route('login') }}">ログインはこちら</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\PurchaseController;
-use App\Http\Controllers\SellController;
-use App\Http\Controllers\MypageController;
-use App\Http\Controllers\ProfileController;
 
-Route::get('/', [ItemController::class, 'index']);
-Route::get('/item/{item_id}', [ItemController::class, 'show']);
-Route::get('/sell', [SellController::class, 'create']);
-Route::get('/mypage', [MypageController::class, 'index']);
-Route::get('/mypage/profile', [ProfileController::class, 'edit']);
-Route::get('/purchase/{item_id}', [PurchaseController::class, 'create']);
-Route::get('/purchase/address/{item_id}', [PurchaseController::class, 'address']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-
-Route::middleware('auth')->group(function () {
-
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
